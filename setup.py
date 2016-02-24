ca89f07bb20c8ac375f2003063c8c20f65d51a6a
Antonin Bas <antonin@barefootnetworks.com>
2016-02-24 09:35:03 -0800

    updated version string in setup.py, even though it does not mean much

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name = 'p4_hlir_v1_1',
-    version = '0.9.29',
+    version = '1.1.0',
     install_requires=['ply'],
     packages=['p4_hlir_v1_1','p4_hlir_v1_1/hlir', 'p4_hlir_v1_1/frontend',
               'p4_hlir_v1_1/util', 'p4_hlir_v1_1/graphs'],
