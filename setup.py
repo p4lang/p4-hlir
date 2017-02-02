26092d29eb822b9314626e709e0da268e91da861
Antonin Bas <antonin@barefootnetworks.com>
2017-02-01 16:39:31 -0800

    Require ply version to be less than 3.10
    We have observed an issue with this version of ply, with the ELSE token
    not being parsed correctly in P4 programs. I am unsure whether this is
    an issue with ply or the p4-hlir code. The best fix right now seems to
    require an older version of ply.

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ class CustomInstallScripts(install_scripts):
 setup(
     name = 'p4_hlir',
     version = '0.9.44',
-    install_requires=['ply'],
+    install_requires=['ply < 3.10'],
     packages=['p4_hlir','p4_hlir/hlir', 'p4_hlir/frontend',
               'p4_hlir/util', 'p4_hlir/graphs'],
     package_data = {
